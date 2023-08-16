5bc4efac93b84e0f77cbeb30941e74e3cd8b6307
Marina Murasaki <117503632+MarinaMurasaki@users.noreply.github.com>
2023-08-16 20:12:25 +1000

    Create style.css

---
+++ css/style.css
@@ -0,0 +1,196 @@
+body {
+  font-size: 20px;
+  border-top: 10px solid purple;
+  width: 1400px;
+  font-family: 'Nunito', sans-serif;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  min-height: 100vh;
+  margin: 0;
+}
+
+a {
+  text-decoration: none;
+  color: #333;
+}
+
+h3 {
+  text-align: center;
+  font-weight: bold;
+}
+
+.logo {
+  text-align: center;
+  margin-bottom: 30px;
+}
+
+.logo img {
+  display: inline-block;
+  margin-bottom: 5px;
+  text-align: center;
+}
+
+header {
+  width: 1200px;
+  margin: 0 auto;
+  margin-bottom: 20px; 
+}
+
+header ul {
+  display: flex;
+  width: 100%;
+  justify-content: center; 
+}
+
+header ul li {
+  width: calc(100% / 3);
+  text-align: center;
+  border-right: 1px solid #ddd;
+}
+
+header ul li a {
+  display: block;
+  padding: 10px; 
+}
+
+header ul li:first-child {
+  border-left: 1px solid #ddd;
+}
+
+.header-image {
+  text-align: center;
+  width: 1200px;
+  padding-bottom: 50px;
+  margin-bottom: 20px;
+}
+
+.container {
+  text-align: center;
+  vertical-align: middle;
+  width: 70%;
+  padding-left: 15%;
+  margin-top: 20px;
+  margin-bottom: 20px; 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+}
+
+.container img {
+  height: auto;
+  padding: 10px;
+}
+
+.intro {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.explanation {
+  text-align: left;
+  padding-left: 20px;
+}
+
+
+.coreValues {
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+  background-color: #f2eded;
+  padding: 10px;
+  width: 100%;
+}
+
+.core-explanation {
+  display: flex;
+  margin: 10px;
+  flex-direction: row;
+  padding: 15px;
+  text-align: center;
+}
+
+.values {
+  width: 33%;
+  padding: 5px;
+}
+
+.values > p {
+  text-align: left;
+}
+
+.goals {
+  display: flex;
+  flex-direction: column;
+  margin: 30px;
+  padding-top: 10px;
+}
+
+footer {
+  background-color: #b3b0b0;
+  display: flex;
+  width: 1400px;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 20px;
+  padding: 20px;
+}
+
+.upper-footer img {
+  height: 30px;
+  width: 30px;
+}
+
+/* For myHistory.html page */
+.timeline {
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  padding: 50px 100px;
+}
+
+.event {
+  display: flex;
+  align-items: flex-start;
+  margin-bottom: 20px;
+}
+
+.event-date {
+  background-color: #333;
+  color: white;
+  padding: 10px;
+  text-align: center;
+  font-weight: bold;
+}
+
+.event-description {
+  background-color: #f2eded;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  margin-left: 10px;
+  width: 70%;
+}
+
+/* For myAdventure Page */
+.lorem {
+  text-align: center;
+  vertical-align: middle;
+  width: 70%;
+  padding-left: 15%;
+  margin-top: 20px;
+  margin-bottom: 20px; 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+}
+
+.lorem > h1 {
+  font-size: 25px;
+  font-weight: bold;
+  padding: 10px;
+}
